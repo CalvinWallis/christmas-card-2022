8fe09b82012b2a39b890777c2824f1419d98db20
Calvin Wallis <calvinwallis@yahoo.com>
2022-12-15 22:14:27 -0800

    do app stuff

--- christmas-card-2022/src/app/app.module.ts
+++ christmas-card-2022/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LetterSquareComponent } from './components/letter-square/letter-square.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetterSquareComponent,
+    HomeComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
