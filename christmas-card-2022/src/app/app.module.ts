a54a016d78b345fae230924d130d21e2bc4ba06f
Calvin Wallis <calvinwallis@yahoo.com>
2022-12-17 15:12:29 -0800

    generate keyboard components

--- christmas-card-2022/src/app/app.module.ts
+++ christmas-card-2022/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LetterSquareComponent } from './components/letter-square/letter-square.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
+import { KeyComponent } from './components/key/key.component';
+import { KeyboardKeyComponent } from './components/keyboard-key/keyboard-key.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LetterSquareComponent,
     HomeComponent,
-    TopNavComponent
+    TopNavComponent,
+    KeyboardComponent,
+    KeyComponent,
+    KeyboardKeyComponent
   ],
   imports: [
     BrowserModule,
---
+++ christmas-card-2022/src/app/components/keyboard-key/keyboard-key.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboard-key',
+  templateUrl: './keyboard-key.component.html',
+  styleUrls: ['./keyboard-key.component.scss']
+})
+export class KeyboardKeyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
