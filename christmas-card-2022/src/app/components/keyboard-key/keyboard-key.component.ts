13d96e0b67f248c585a4fddfd753d221bd84af01
Calvin Wallis <calvinwallis@yahoo.com>
2022-12-17 16:16:21 -0800

    Keyboard creation

--- christmas-card-2022/src/app/components/keyboard-key/keyboard-key.component.ts
+++ christmas-card-2022/src/app/components/keyboard-key/keyboard-key.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-keyboard-key',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./keyboard-key.component.scss']
 })
 export class KeyboardKeyComponent implements OnInit {
+  @Input() keyName?: string;
+  @Input() largeKey: boolean = false;
+  @HostBinding('class.large') get isLarge() {
+    return this.largeKey;
+  }
+
+  @Input() clear: boolean = false;
+  @HostBinding('class.clear') get isClear() {
+    return this.clear;
+  }
+
+  @Output() keyPressed: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  keyPress(value: string | undefined): void {
+    if(value){
+      this.keyPressed.emit(value);
+    }
+  }
 }
