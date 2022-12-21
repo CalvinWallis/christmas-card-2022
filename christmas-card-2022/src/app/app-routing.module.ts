dfbcb1e0e4929439ec64cff1ed71d2ffabcdc341
Calvin Wallis <calvinwallis@yahoo.com>
2022-12-20 20:54:01 -0800

    move card component to page folder

--- christmas-card-2022/src/app/app-routing.module.ts
+++ christmas-card-2022/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CardComponent } from './components/card/card.component';
+import { CardComponent } from './pages/card/card.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
--- christmas-card-2022/src/app/app.module.ts
+++ christmas-card-2022/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LetterSquareComponent } from './components/letter-square/letter-square.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './pages/card/card.component';
+
+import { LetterSquareComponent } from './components/letter-square/letter-square.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
 import { KeyboardKeyComponent } from './components/keyboard-key/keyboard-key.component';
 import { LetterRowComponent } from './components/letter-row/letter-row.component';
-import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
