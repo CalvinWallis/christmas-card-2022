32d676ceaa3dc5fa253a7e7e142ac534d55b4a38
Calvin Wallis <calvinwallis@yahoo.com>
2022-12-21 22:53:41 -0800

    add card and stuff

--- christmas-card-2022/src/app/pages/card/card.component.ts
+++ christmas-card-2022/src/app/pages/card/card.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  clickedFam: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cardClick(target: string) {
+    this.clickedFam = target;
+  }
 }
